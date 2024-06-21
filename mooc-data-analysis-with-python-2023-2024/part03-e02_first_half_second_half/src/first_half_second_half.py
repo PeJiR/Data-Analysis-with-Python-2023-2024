d384b792c56c3e71977cd6ebb6fe26ca37376a3e
Roberto Perez <129693538+PeJiR@users.noreply.github.com>
2024-06-21 13:38:52 +0200

    update

--- mooc-data-analysis-with-python-2023-2024/part03-e02_first_half_second_half/src/first_half_second_half.py
+++ mooc-data-analysis-with-python-2023-2024/part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -3,10 +3,19 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    # Determine the middle_indexdle index
+    middle_index = int(a.shape[1] / 2)
+
+    # Determine if the first half is larger than the second half
+    greater_than = np.sum(a[:,:middle_index],1)> np.sum(a[:,middle_index:],1)
+
+    return a[greater_than]
+
 
 def main():
-    pass
+    a = np.array([[1, 3, 4, 2], [2, 2, 1, 2]])
+    print(first_half_second_half(a))
+
 
 if __name__ == "__main__":
     main()

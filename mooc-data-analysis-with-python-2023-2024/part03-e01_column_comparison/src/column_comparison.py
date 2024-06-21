ad04cbbaa185d0ba0605c8a74c4b5fd72fc06b86
Roberto Perez <129693538+PeJiR@users.noreply.github.com>
2024-06-21 12:57:28 +0200

    Update column_comparison.py

--- mooc-data-analysis-with-python-2023-2024/part03-e01_column_comparison/src/column_comparison.py
+++ mooc-data-analysis-with-python-2023-2024/part03-e01_column_comparison/src/column_comparison.py
@@ -2,14 +2,26 @@
 
 import numpy as np
 
-import pandas as pd
-a=pd.read_csv("https://raw.githubusercontent.com/csmastersUH/data_analysis_with_python_2020/master/kumpula-weather-2017.csv")['Air temperature (degC)'].values
-
 def column_comparison(a):
+    return a[a:,1] > a[:, -2]
+    
+    
     return np.array([])
     
 def main():
-    pass
+    
+    a = np.array(
+        [
+            [8, 9, 3, 8, 8],
+            [0, 5, 3, 9, 9],
+            [5, 7, 6, 0, 4],
+            [7, 8, 1, 6, 2],
+            [2, 1, 3, 5, 8],                             
+            
+        ]
+    )
+    print(column_comparison(a))
+   
 
 if __name__ == "__main__":
     main()

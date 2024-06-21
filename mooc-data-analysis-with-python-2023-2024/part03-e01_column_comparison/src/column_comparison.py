2ed2b8df1065293fceefc8917f0b0dfa021d5c01
Roberto Perez <129693538+PeJiR@users.noreply.github.com>
2024-06-21 11:48:54 +0200

    Update column_comparison.py

--- mooc-data-analysis-with-python-2023-2024/part03-e01_column_comparison/src/column_comparison.py
+++ mooc-data-analysis-with-python-2023-2024/part03-e01_column_comparison/src/column_comparison.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 
+import pandas as pd
+a=pd.read_csv("https://raw.githubusercontent.com/csmastersUH/data_analysis_with_python_2020/master/kumpula-weather-2017.csv")['Air temperature (degC)'].values
+
 def column_comparison(a):
     return np.array([])
     
